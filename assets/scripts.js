77ec875031f1f573422f2525005f142303cb5534
Sushovan Majhi <sush@tulane.edu>
2017-05-04 11:35:53 -0500

    added headers

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,22 +1,11 @@
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-82908020-1', 'auto');
-ga('send', 'pageview');
-
-
-
 $(document).ready(
     function() {
 	var docHeight = $(document).height(),
 	    scrollPercent;
-
+	
 	$(window).scroll(function() {
 	    scrollPercent = ($(window).scrollTop() / docHeight) * 100;
-
+	    
 	    $('.scroll-progress').width(scrollPercent + '%');
 	});
     });
@@ -25,10 +14,47 @@ $(document).ready(function() {
     var docHeight = $(document).height(),
 	windowHeight = $(window).height(),
 	scrollPercent;
-
+    
     $(window).scroll(function() {
 	scrollPercent = $(window).scrollTop() / (docHeight - windowHeight) * 100;
 	
 	$('.scroll-progress').width(scrollPercent + '%');
     });
 });
+
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+	// Make sure this.hash has a value before overriding default behavior
+	if (this.hash !== "") {
+	    // Prevent default anchor click behavior
+	    event.preventDefault();
+	    
+	    // Store hash
+		var hash = this.hash;
+	    
+	    // Using jQuery's animate() method to add smooth page scroll
+	    // The optional number (900) specifies the number of milliseconds it takes to scroll to the s\
+	    pecified area
+	    $('html, body').animate({
+		scrollTop: $(hash).offset().top
+	    }, 900, function(){
+		
+		// Add hash (#) to URL when done scrolling (default click behavior)
+		window.location.hash = hash;
+	    });
+	} // End if
+    });
+    
+    $(window).scroll(function() {
+	$(".slideanim").each(function(){
+	    var pos = $(this).offset().top;
+	    
+	    var winTop = $(window).scrollTop();
+	    if (pos < winTop + 600) {
+		$(this).addClass("slide");
+	    }
+	});
+    });
+})
